00e37fca6e9faa2fbd0d16a7facde09100117320
Bruno Bibbó <brubicalata@gmail.com>
2020-11-10 23:25:21 +0100

    Player Attack Controller
    Scripts created:
    * Player Controller - Holds click action and attack to an enemy by clicking on it
    * Enemy Controller - Holds enemy lifes and destroys itself on empty lifes
    
    Prefabs created:
    * Enemy - Rocket
    * Luna - Player
    
    Tags created:
    * Enemy
    
    Scripts removed:
    * Player Manager

--- Assets/Scripts/Enemies/EnemyController.cs
+++ Assets/Scripts/Enemies/EnemyController.cs
@@ -2,17 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerManager : MonoBehaviour
+public class EnemyController : MonoBehaviour
 {
+    public int lives;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        lives = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (lives <= 0)
+            Destroy(this.gameObject);
+    }
+
+    public void RemoveLife()
+    {
+        lives--;
     }
 }
