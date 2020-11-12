6200fdc5f84eb24d636388f40bbe917a700ee8cb
Bruno Bibbó <brubicalata@gmail.com>
2020-11-13 00:54:45 +0100

    Hands Attack Behaviour
    Added following attack functionalities:
    * Player now can attack with hands (Left / Right) by clicking on screen.
    * Every hand has one attack at a time, in its screen area (Left screen area / Right screen area).
    * Hands can kill as many as rockets as it collides with while is traveling forward to clicked position.
    * Added basic temp animations to hands (may change at any time).
    
    Removing some unnecesary files and solving warnings.
    
    Changed project folders organization to tidy up assets.

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public HandController leftHand;
+    public HandController rightHand;
+
+    float halfScreenWidth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        halfScreenWidth = Screen.width / 2.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePos = Input.mousePosition;
+            Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(mousePos);
+            Vector2 mouseWorldPos2D = new Vector2(mouseWorldPos.x, mouseWorldPos.y);
+
+            if (mousePos.x <= halfScreenWidth)
+                leftHand.Attack(mouseWorldPos2D);
+            else
+                rightHand.Attack(mouseWorldPos2D);
+        }
+    }
+}
