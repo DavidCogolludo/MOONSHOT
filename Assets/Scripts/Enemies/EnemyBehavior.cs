e2f73787e15901196ff1085ae7a0956e9e3f3891
Jorge Gomez Weyler <jgweyler@gmail.com>
2020-11-21 14:23:37 +0000

    Cambiando velocidad al aterrizar

--- Assets/Scripts/Enemies/EnemyBehavior.cs
+++ Assets/Scripts/Enemies/EnemyBehavior.cs
@@ -7,13 +7,24 @@ using UnityEngine;
 
 public class EnemyBehavior : MonoBehaviour
 {
-    public float speed = 0.5f;
-    public Vector3 target_pos = new Vector3(0.0f, 0.0f, 0.0f); // The moon pos
-    public float min_distance_for_landing = 10.0f;
+    // CONSTANTS
+    private const float angle_offset = -90;
+    private const float MAX_LANDING_ANGLE = 180.0f;
 
-    // Maybe we have to change this offset depending the position/rotation of the sprite to rotate it properly.
-    private float angle_offset = -90;
+    // Global params
+    public float speed = 10.0f;
+    private bool has_landed = false;
+
+    //Moon info
+    // TODO: Get the moon radius and position on start.
+    public Vector3 target_pos = new Vector3(0.0f, 0.0f, 0.0f); // The moon pos
+    public float moon_radius = 2.7f;
+
+    // Landing params
+    public float min_distance_for_landing = 10.0f;  // The min distance between the enemy and the moon to start landing
     private float current_land_rotation = 0.0f;
+    private float land_speed;
+    
 
     // Start is called before the first frame update
     void Start()
@@ -21,21 +32,27 @@ public class EnemyBehavior : MonoBehaviour
         Vector3 dir = target_pos - transform.position;
         float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg + angle_offset;
         transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-        float moon_radius = 2.7f;
-        target_pos = new Vector3((float)(moon_radius * Mathf.Cos(angle)), (float)(moon_radius * Mathf.Sin(angle)), 0.0f);
+
+        // Initialize land speed proportionaly of the current speed
+        land_speed = speed / 2.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        if (has_landed)
+        {
+            return;
+        }
+
         float current_speed = Time.deltaTime * speed;
-        Vector3 new_velocity_vector = (target_pos - transform.position) * current_speed;
-        transform.position += new_velocity_vector;
+        Vector3 normalized_dir = (target_pos - transform.position).normalized;
+        transform.position = transform.position + normalized_dir * current_speed;
 
-        float distance_from_target = Vector3.Distance(transform.position, target_pos);
-
+        float distance_from_target = Vector3.Distance(transform.position, target_pos) - moon_radius;
         // If we reach the minimum distance between the object and the target -> Start landing
-        if (distance_from_target <= min_distance_for_landing)
+        if (distance_from_target <= (min_distance_for_landing))
         {
             land(distance_from_target);
         }
@@ -44,15 +61,19 @@ public class EnemyBehavior : MonoBehaviour
     void land(float distance_from_target)
     {
         //Debug.Log(distance_from_target);
-        if (distance_from_target <= 0 || current_land_rotation >= 180.0f)
+        if (distance_from_target <= 0.01f || current_land_rotation >= MAX_LANDING_ANGLE)
         {
+            has_landed = true;
             return;
         }
 
-        float max_rotation = 180.0f;
+        if (speed != land_speed)
+        {
+            speed = land_speed;
+        }
 
         float percent_to_land = ((min_distance_for_landing - distance_from_target)/ min_distance_for_landing) * 100.0f;
-        float current_rotation = (max_rotation * percent_to_land)/100.0f;
+        float current_rotation = (MAX_LANDING_ANGLE * percent_to_land)/100.0f;
         float actual_rotation_to_increase = current_rotation - current_land_rotation;
         transform.Rotate(Vector3.forward, actual_rotation_to_increase);
         current_land_rotation += actual_rotation_to_increase;
