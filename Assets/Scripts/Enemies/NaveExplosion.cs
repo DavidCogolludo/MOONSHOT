f0bcb6bf57c5caf280669cfe0357f2e402085417
Carco1992 <dcarcov@hotmail.com>
2020-11-27 20:48:00 +0100

    Scripts nave explosion

---
+++ Assets/Scripts/Enemies/NaveExplosion.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NaveExplosion : MonoBehaviour
+{
+    private Rigidbody2D rigidbody2D;
+    private Vector3 position;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.tag == "Player")
+        {
+            position = collision.transform.position;
+            rigidbody2D.AddForce((transform.position - position) * 0.5f, ForceMode2D.Impulse);
+        }
+    }
+}
